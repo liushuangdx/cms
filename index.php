ec35df4525355c8ed3d18aa8c996e115d082cda9
liushuangdx <1597633078@qq.com>
2015-01-18 22:24:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-//import constant
+// import constant
 require_once (dirname(__FILE__).'/protected/config/constant.php');
 require_once($yii);
 Yii::createWebApplication($config)->run();
